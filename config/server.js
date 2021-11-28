230ce68eacd69489ba63c107bfd5aa30ed572029
davidhanenko <d.hanenko24@gmail.com>
2021-11-28 03:38:45 -0500

    init

--- config/server.js
+++ config/server.js
@@ -3,7 +3,10 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env(
+        'ADMIN_JWT_SECRET',
+        '6fvebab2-434gfd6-47u6h-8fghsfr-bfhsh6ga51sfds'
+      ),
     },
   },
 });
