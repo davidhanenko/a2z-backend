5b82bdb09c3d9e6ce349fa562f64a8e64b0df5dd
davidhanenko <d.hanenko24@gmail.com>
2021-12-02 22:40:50 -0500

    fixing 405, cors

--- config/middleware.js
+++ config/middleware.js
@@ -5,7 +5,7 @@ module.exports = {
       headers: '*',
       origin: [
         'https://a2z-frontend-demo.vercel.app',
-        'https://a2z-demo.herokuapp.com',
+        'https://a2z-demo.herokuapp.com/',
       ],
     },
   },
