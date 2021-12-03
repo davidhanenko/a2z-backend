9a7b2abebdb50dd81fa756087b377c80982fcba9
davidhanenko <d.hanenko24@gmail.com>
2021-12-02 20:58:56 -0500

    fixing 405, cors

--- config/middleware.js
+++ config/middleware.js
@@ -1,21 +1,21 @@
-module.exports = {
-      settings: {
-        cors: {
-          origin: [
-            'https://a2z-demo.herokuapp.com',
-            'https://a2z-frontend-demo.vercel.app',
-          ],
-        },
-        // 10 Mb max upload file size
-        parser: {
-          enabled: true,
-          multipart: true,
-          formidable: {
-            maxFileSize: 10 * 1024 * 1024,
-          },
-        },
-      },
-    };
+// module.exports = {
+//       settings: {
+//         cors: {
+//           origin: [
+//             'https://a2z-demo.herokuapp.com',
+//             'https://a2z-frontend-demo.vercel.app',
+//           ],
+//         },
+//         // 10 Mb max upload file size
+//         parser: {
+//           enabled: true,
+//           multipart: true,
+//           formidable: {
+//             maxFileSize: 10 * 1024 * 1024,
+//           },
+//         },
+//       },
+//     };
   
 
   // return {
