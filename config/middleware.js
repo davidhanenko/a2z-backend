7d3827bb3aec3dbbe2effb8c7b05ca575315adef
davidhanenko <d.hanenko24@gmail.com>
2021-12-06 21:02:33 -0500

    database setting

--- config/middleware.js
+++ config/middleware.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => {
           enabled: false,
           origin: [
             'https://a2z-frontend-demo.vercel.app',
-            'https://a2z-demo.herokuapp.com/','http://localhost:1337', 'http://localhost:7777'
+            'https://a2z-demo.herokuapp.com','http://localhost:1337', 'http://localhost:7777'
           ],
         },
       
