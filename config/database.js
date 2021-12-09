d4b69e988603f73bbe381ad96915c9e0f77a44d3
davidhanenko <d.hanenko24@gmail.com>
2021-12-09 00:53:23 -0500

    db

--- config/database.js
+++ config/database.js
@@ -15,10 +15,10 @@ module.exports = ({ env }) => {
             database: config.database,
             username: config.user,
             password: config.password,
-            // ssl: {
-            //   rejectUnauthorized: false,
-            //   // rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-            // },
+            ssl: {
+              // rejectUnauthorized: false,
+              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+            },
           },
           options: {
             ssl: false,
