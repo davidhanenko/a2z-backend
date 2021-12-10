156bbbbbb3493852929f19962272853e64c43ca5
davidhanenko <d.hanenko24@gmail.com>
2021-12-09 23:17:26 -0500

    cors

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -2,11 +2,18 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      // origin: ['https://a2z-frontend-demo.vercel.app',
-      //       'https://a2z-demo.herokuapp.com',
-      //       'http://localhost:1337',
-      //       'http://localhost:7777',],
-      headers: '*',
+      origin: ['https://a2z-frontend-demo.vercel.app',
+            'https://a2z-demo.herokuapp.com',
+            'http://localhost:1337',
+            'http://localhost:7777'],
+    },
+    // 10 Mb max upload file size
+    parser: {
+      enabled: true,
+      multipart: true,
+      formidable: {
+        maxFileSize: 10 * 1024 * 1024
+      },
     },
   },
 };
--- config/middleware.js
+++ config/middleware.js
@@ -1,18 +1,4 @@
-module.exports = ({ env }) => {
-  if (env('NODE_ENV') === 'production') {
-    return {
-      settings: {
-        cors: {
-          origin: [
-            'https://a2z-frontend-demo.vercel.app',
-            'https://a2z-demo.herokuapp.com','http://localhost:1337', 'http://localhost:7777'
-          ],
-        },
-      },
-    };
-  }
-
-  return {
+module.exports = {{
     settings: {
       cors: {
         origin: ['http://localhost:1337', 'http://localhost:7777'],
