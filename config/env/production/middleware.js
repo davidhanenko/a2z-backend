3e97200e48f51212811b6f444114148d8a999aea
davidhanenko <d.hanenko24@gmail.com>
2021-12-09 22:14:25 -0500

    cors

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -1,11 +1,13 @@
 module.exports = {
   settings: {
     cors: {
-      enabled: true,
-      origin: ['https://a2z-frontend-demo.vercel.app',
-            'https://a2z-demo.herokuapp.com',
-            'http://localhost:1337',
-            'http://localhost:7777',],
+      // enabled: true,
+      // origin: ['https://a2z-frontend-demo.vercel.app',
+      //       'https://a2z-demo.herokuapp.com',
+      //       'http://localhost:1337',
+      //       'http://localhost:7777',],
+      origin: '*',
+      headers: '*',
     },
   },
 };
