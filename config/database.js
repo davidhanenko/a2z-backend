3868703b5f9a851cb8728ca801cec11783cc8d27
davidhanenko <d.hanenko24@gmail.com>
2021-12-09 20:09:27 -0500

    db deploy config

--- config/database.js
+++ config/database.js
@@ -1,51 +1,18 @@
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-  if (env('NODE_ENV') === 'production') {
-    const config = parse(process.env.DATABASE_URL);
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-            ssl: {
-              // rejectUnauthorized: false,
-              rejectUnauthorized: false,
-            },
-          },
-          // options: {
-          //   ssl: true,
-          //   // ssl: env.bool('DATABASE_SSL', false),
-          // },
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'a2z'),
+        username: env('DATABASE_USERNAME', 'david'),
+        password: env('DATABASE_PASSWORD', 'qqqwww123'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
-      debug: false,
-    };
-  }
-
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'postgres',
-          host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          username: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          ssl: env.bool('DATABASE_SSL', false),
-        },
-        options: {},
-      },
+      options: {},
     },
-  };
-};
+  },
+});
