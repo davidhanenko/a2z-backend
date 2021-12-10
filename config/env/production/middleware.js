6b1b3fab422608a01011820534a171de3220851a
davidhanenko <d.hanenko24@gmail.com>
2021-12-09 21:37:00 -0500

    cors

---
+++ config/env/production/middleware.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //...
+  settings: {
+    cors: {
+      origin: [
+        'https://a2z-frontend-demo.vercel.app',
+        'https://a2z-demo.herokuapp.com',
+      ],
+    },
+  },
+};
