54f0ea4eecc4265776e116b0aae661b9613ea276
davidhanenko <d.hanenko24@gmail.com>
2021-12-11 14:40:25 -0500

    test

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -4,6 +4,8 @@ module.exports = {
       origin: [
         'https://a2z-frontend-demo.vercel.app',
         'https://a2z-demo.herokuapp.com',
+        'http://localhost:1337',
+        'http://localhost:7777',
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
     },
