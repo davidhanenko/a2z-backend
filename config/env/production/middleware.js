2434a2f6baeb2432cb37968ee377575726b50dec
davidhanenko <d.hanenko24@gmail.com>
2021-12-10 23:38:59 -0500

    test

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -1,21 +1,11 @@
 module.exports = {
   settings: {
     cors: {
-      enabled: true,
       origin: [
         'https://a2z-frontend-demo.vercel.app',
         'https://a2z-demo.herokuapp.com',
       ],
-      credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
-    },
-    // 10 Mb max upload file size
-    parser: {
-      enabled: true,
-      multipart: true,
-      formidable: {
-        maxFileSize: 10 * 1024 * 1024,
-      },
     },
   },
 };
