dd1263991686173a1a3c96ee6cedb91f889cb6a3
davidhanenko <d.hanenko24@gmail.com>
2021-12-11 14:54:22 -0500

    test

--- config/plugins.js
+++ config/plugins.js
@@ -1,27 +1,25 @@
-module.exports = ({ env }) => {
-  return {
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      breakpoints: {
-        xsmall: 320,
-      },
-      actionOptions: {
-        upload: {
-          transformation: {
-            width: 1920,
-          },
-          fetch_format: 'auto',
-          quality: 'auto',
-          resource_type: 'auto',
-          folder: 'A2Z',
-        },
-        delete: {},
-      },
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
     },
-  };
-};
+    breakpoints: {
+      xsmall: 320,
+    },
+    actionOptions: {
+      upload: {
+        transformation: {
+          width: 1920,
+        },
+        fetch_format: 'auto',
+        quality: 'auto',
+        resource_type: 'auto',
+        folder: 'A2Z',
+      },
+      delete: {},
+    },
+  },
+});
