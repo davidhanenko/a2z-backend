0e742d3975b2f457d6461f190f594f966b140525
davidhanenko <d.hanenko24@gmail.com>
2021-12-10 22:30:26 -0500

    cors

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -1,21 +1,21 @@
-module.exports = {
-  settings: {
-    cors: {
-      enabled: true,
-      origin: '*',
-      expose: ['WWW-Authenticate', 'Server-Authorization'],
-      maxAge: 31536000,
-      credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-      headers: ['Content-Type', 'Authorization', 'X-Frame-Options', 'Origin'],
-    },
-    // 10 Mb max upload file size
-    parser: {
-      enabled: true,
-      multipart: true,
-      formidable: {
-        maxFileSize: 10 * 1024 * 1024,
-      },
-    },
-  },
-};
+// module.exports = {
+//   settings: {
+//     cors: {
+//       enabled: true,
+//       origin: '*',
+//       expose: ['WWW-Authenticate', 'Server-Authorization'],
+//       maxAge: 31536000,
+//       credentials: true,
+//       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+//       headers: ['Content-Type', 'Authorization', 'X-Frame-Options', 'Origin'],
+//     },
+//     // 10 Mb max upload file size
+//     parser: {
+//       enabled: true,
+//       multipart: true,
+//       formidable: {
+//         maxFileSize: 10 * 1024 * 1024,
+//       },
+//     },
+//   },
+// };
