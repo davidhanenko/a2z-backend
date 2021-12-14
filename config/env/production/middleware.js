d2dd066ce377b86b605d3e8f9ec5b9bff1300348
davidhanenko <d.hanenko24@gmail.com>
2021-12-13 20:17:23 -0500

    updated db

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -3,7 +3,7 @@ module.exports = {
     cors: {
       origin: [
         'https://a2z-frontend-demo.vercel.app',
-        'https://a2z-demo.herokuapp.com',
+        'https://a2z-backend-demo.herokuapp.com',
         'http://localhost:1337',
         'http://localhost:7777',
       ],
