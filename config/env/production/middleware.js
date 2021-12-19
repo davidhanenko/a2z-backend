b5c2cdb518beb8f052d58fe4b210cf80bdb09144
davidhanenko <d.hanenko24@gmail.com>
2021-12-19 16:47:18 -0500

    do deploy, lock

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -3,7 +3,7 @@ module.exports = {
     cors: {
       origin: [
         'https://a2z-frontend-demo.vercel.app',
-        'https://backend-a2z.',
+        'https://backend-a2z-kup9t.ondigitalocean.app',
         'http://localhost:1337',
         'http://localhost:7777',
       ],
